93b499f650cc324deb056daed3d7d9ea5243cea0
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2020-06-28 20:38:31 -0700

    Added authentication and authorization

---
+++ src/main/java/com/data/tracker/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.data.tracker.repository;
+
+import com.data.tracker.model.ERole;
+import com.data.tracker.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
---
+++ src/main/java/com/data/tracker/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.data.tracker.security.services;
+
+import com.data.tracker.model.User;
+import com.data.tracker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " +username));
+        return UserDetailsImpl.build(user);
+    }
+}
